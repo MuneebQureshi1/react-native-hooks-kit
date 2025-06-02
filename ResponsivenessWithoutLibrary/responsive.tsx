72589054bcbb06c57510fcd9eb9f176336c5f61e
Muneeb-Dev-Ikonic <muneeb@ikonicsolution.com>
2025-06-03 00:04:43 +0500

    Add: Responsiveness functions are added without library

---
+++ ResponsivenessWithoutLibrary/responsive.tsx
@@ -0,0 +1,25 @@
+/**
+ * @fileoverview Responsive design utilities
+ * @description This file contains the responsive design utilities for the app
+ * @author Muneeb Qureshi
+ */
+
+import { Dimensions, Platform } from "react-native";
+
+export const width = Math.round(Dimensions.get("window").width);
+export const height = Math.round(Dimensions.get("window").height);
+
+//Guideline sizes are based on standard ~5" screen mobile devices
+const guidelineBaseWidth = 360;
+const guidelineBaseHeight = 800 + 40;
+
+export const scale = (size: any) => (width / guidelineBaseWidth) * size;
+
+export const vs = (size: any) => (height / guidelineBaseHeight) * size;
+
+export const ms = (size: any, factor: any = 0.5) => size + (scale(size) - size) * factor;
+
+export const mvs = (size: number, factor: number = Platform.OS === "android" ? 0.7 : 0.5) => {
+  const result = size + (vs(size) - size) * factor;
+  return Math.round(result * 100) / 100;
+};
